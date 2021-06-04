06df1aad015339637c46e99ddae3d702906197fc
Eliotdoesprogramming <zubdoob@gmail.com>
2021-06-04 09:04:38 -0500

    relationship cleanup

--- app/main/model/employee.py
+++ app/main/model/employee.py
@@ -3,14 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from model import db,ma
 from sqlalchemy.sql.schema import ForeignKey
-from model import education
+from model.education import Education
+from model.exp_model import Experience
+from model.job import Job
+
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     job =  db.Column(db.String(100))
     # ed_id = db.Column(db.Integer, db.ForeignKey('education.id'))
+    job = db.relationship("Job", uselist=False, backref="employee")
     educations = db.relationship('Education', backref='employee')
+    experiences = db.relationship('Experience', backref='experience')
 
 
 
@@ -22,6 +27,7 @@ class Employee(db.Model):
 
 #Temp Employee Schema
 
-class EmployeeSchema(ma.Schema):
+class EmployeeSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ('id','name','job')
+        model=Employee
+        
