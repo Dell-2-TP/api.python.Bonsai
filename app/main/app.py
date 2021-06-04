cb47aba5026c6286b44e42e7582910695bd83879
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-06-04 10:55:52 -0400

    completed swagger documentation

--- app/main/app.py
+++ app/main/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from service.api_service import Service
 from controller.sample_controller import SampleController
 from model import db
-
+from flask_swagger_ui import get_swaggerui_blueprint
 #init application context
 app = Flask(__name__)
 
@@ -12,11 +12,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password123@localhost:5432/Bonsai'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Bonsai"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
 #init service, routes. might refactor to init multiple with a single initialization
 service = Service(app,db)
 sample_controller = SampleController(app,service)
+
 
 if(__name__=='__main__'):
     db.init_app(app)
